e4560117daf4e3ef29f206e4b4df85d604f832bb
ploszukiwacz <87446593+ploszukiwacz@users.noreply.github.com>
2025-03-30 16:02:15 +0200

    beeeeeep

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Beep.
 
 [discord invite link](https://discord.com/oauth2/authorize?client_id=1355840509322727554&permissions=1747121895636800&integration_type=0&scope=bot)
+
+## How to setup
+1. Copy `.env.example` to `.env`
+2. Set all the fields in `.env`
+3. `docker compose up -d`
+4. Wait
+5. ???
+6. Profit
--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from discord.ext import commands
 
 load_dotenv()
 
+chance = 100
+
+if os.getenv('DEBUG') == 'true':
+    chance = 1
+
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='b!', intents=intents)
@@ -19,7 +24,8 @@ async def on_message(message: discord.Message):
     if message.author.bot:
         return
     
-    if random.randint(1, 100) == 1:
+    if random.randint(1, chance) == 1:
+        print(f'{message.author.name} ({message.author.id}) triggered a Beep in {message.channel.id}, server {message.guild.id}')
         await message.channel.send("Beep.")
 
 bot.run(os.getenv('TOKEN'))
